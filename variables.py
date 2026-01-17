06b2bd450b87997ac70f8e6c869ed7f0443e8178
Fongu <ngufonakips@gmail.com>
2026-01-17 15:43:44 -0500

    t add#	fan_key26.pub

---
+++ variables.py
@@ -0,0 +1,32 @@
+# Create 4 variables for traveling. flight_cost, insurance, passport, and baggage
+#Cretae a variable to calculate the cost of all of them
+#print off the total cost with strings and variables
+# Note: there are 5 people travelling
+flight_cost = 455
+insurance = 19.99
+passport_fees = 110
+baggage = 45
+Total = (flight_cost + insurance + passport_fees + baggage)*5
+#print("Total cost:",Total)
+
+#Create 4 variables for ticket price. 1 variable is for the number of adults & 1 for children
+#create 1 variable for the adult price and 1 for the child price
+#Create a variable to calculate the total
+#Print off the total with strings and variables
+adult_price = 55
+child_price = 27
+adult = 2
+child = 2
+
+total = (adult_price*2) + (child_price*2)
+#print("The total cost for your flight is:" ,total)
+
+# A resort offers the normal price of 500 pernight, a family wants to stay for one week. The resort is ofering a special prmo of 30% off
+#print off the last minute trip price with strings and variables.
+
+price =50
+days = 7
+promo = 30*350/100
+total = (price*days) - promo
+#print("the last minute special price is :", total)
+
