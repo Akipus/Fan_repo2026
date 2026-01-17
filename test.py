26ed3e91af9da7ab880bb36c6e22b36ed8f79181
Fongu <ngufonakips@gmail.com>
2026-01-16 22:29:35 -0500

    my test file

---
+++ test.py
@@ -0,0 +1,18 @@
+print(" I am the man who can talk to you")
+print("meet you " + "the young  man")
+print(3*7)
+print(21%2)
+print(22//3)
+print(4**4)
+print("Funds to send", 200*5 + 50/2 +750-75)
+print(57 % 2)
+print(30/9)
+print(30//9)
+years = 2
+months = 6
+print("I have bee with the company for", years, "years and", months, "months.")
+base_fare = 155
+num_of_bags = 2
+base_fare = 205
+Total = base_fare*(num_of_bags*0.75)
+print(Total)
