ef01cd95d4531f3f7d6871fe467853bcfec8253e
Fongu <ngufonakips@gmail.com>
2026-01-16 22:01:00 -0500

    first commit

---
+++ hello.py
@@ -0,0 +1,5 @@
+# ask user for their names
+name = input("what is your name? ")
+print("hello, " + name + " how are you and family doing today?")
+print("hello,", name, sep=" ")
+print("Hello "\friend\"")
