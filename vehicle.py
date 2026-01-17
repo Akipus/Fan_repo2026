cf3b7610c68bc05458959b668168f096100a9df6
Fongu <ngufonakips@gmail.com>
2026-01-17 16:25:00 -0500

    commiting vehicles info

---
+++ vehicle.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+from pyodide.http import pyfetch
+async def download(url, filename):
+    response = await pyfetch(url)
+    if response.status == 200:
+        with open(filename, "wb") as f:
+            f.write(await response.bytes())
+
+
+
