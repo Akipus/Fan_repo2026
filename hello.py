33e138a34f8b568fb07ae9a26e78d53233c01b4e
Fongu <47017337+Akipus@users.noreply.github.com>
2026-01-17 19:45:03 -0500

    Added a message in hello.py
    We have to discuss about this code.

--- hello.py
+++ hello.py
@@ -3,3 +3,4 @@ name = input("what is your name? ")
 print("hello, " + name + " how are you and family doing today?")
 print("hello,", name, sep=" ")
 print("Hello "\friend\"")
+#There is much to talk about before the end of the year.
